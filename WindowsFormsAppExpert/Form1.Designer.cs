b234db14d0e41003b858d7acc8bd1e5992ad5c13
Relarek <117584900+Relarek@users.noreply.github.com>
2023-01-03 21:52:49 +0300

    AppExpert

---
+++ WindowsFormsAppExpert/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace WindowsFormsAppExpert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Items.AddRange(new object[] {
+            "Камень",
+            "Ножницы",
+            "Бумага",
+            "Такой же элемент"});
+            this.listBox1.Location = new System.Drawing.Point(50, 112);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(149, 64);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(503, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Peru;
+            this.button1.Location = new System.Drawing.Point(287, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 64);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Кто победит?";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Bisque;
+            this.label2.Location = new System.Drawing.Point(47, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(287, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Кто сильнее в игре \"Камень, ножницы, бумага\"?";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(735, 285);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
+            this.Text = "Кулинария";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsAppExpert/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppExpert
+{
+    public partial class Form1 : Form
+    {
+        /*
+        01  Молоко      
+        02  Картофель
+        03  Масло
+        04  Яйца
+        05  Мясо
+        06  Мука        
+        07  Лук
+        08  Морковь
+        09  Капуста
+        10  Вода
+        */
+        public Dictionary<int, string> Dish; 
+
+        // Add some elements to the dictionary. There are no
+        // duplicate keys, but some of the values are duplicates.
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int id = 0;
+            foreach (int listBoxItem in listBox1.SelectedIndices)
+            {
+                id |= (1 << (listBoxItem));
+            }
+            
+            try
+            {
+                label1.Text = id.ToString() + " " + Dish[id]; 
+            }
+            catch (KeyNotFoundException)
+            {
+                label1.Text = id.ToString() + " " + "Ничья";
+            }            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Dish = new Dictionary<int, string>();
+            Dish.Add(3, "Победит камень");
+            Dish.Add(6, "Победят ножницы");
+            Dish.Add(5, "Победит бумага");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
